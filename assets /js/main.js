185b65e4aa702b4f018839c99a3650482e60809c
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-07-15 00:48:14 +0200

    commit

---
+++ assets /js/main.js
@@ -0,0 +1,12 @@
+/* let score = 5 + 5 * 10;
+console.log('Ergebnis: ' + score); */
+
+/* let score = (5 + 5) * 10;
+console.log('Ergebnis: ' + score); */
+
+/* let score = 0;
+score = score + 10;
+console.log('Ergebnis: ' + score); */
+
+score += 10;
+console.log('Ergebnis: ' + score)
